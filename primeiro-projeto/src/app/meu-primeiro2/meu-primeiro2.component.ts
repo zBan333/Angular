4cb93ed0bc9f66e11ee186503497532828955e96
Vitor Eble <vitoreble06@gmail.com>
2024-08-21 17:37:55 -0300

    aula 04

---
+++ primeiro-projeto/src/app/meu-primeiro2/meu-primeiro2.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meu-primeiro2',
+  templateUrl: './meu-primeiro2.component.html',
+  styleUrl: './meu-primeiro2.component.scss'
+})
+export class MeuPrimeiro2Component {
+
+
+}
