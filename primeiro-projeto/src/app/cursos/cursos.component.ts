8b2570117995e38822c06d9bf099a444b07f912f
Vitor Eble <vitoreble06@gmail.com>
2024-08-22 18:28:26 -0300

    service e DI

--- primeiro-projeto/src/app/cursos/cursos.component.ts
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CursosService } from './cursos.service';
+
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
@@ -9,14 +11,18 @@ export class CursosComponent implements OnInit {
 
   nomePortal: string;
 
-  cursos: string[] = ['java', 'Ext JS', 'Angular'];
+  cursos: string[];
 
-  constructor() {
+
+  constructor(private cursosService: CursosService) {
     this.nomePortal = 'http://loiane.training';
 
-    for (let i=0; i<this.cursos.length; i++){
-      let curso = this.cursos[i];
-    }
+    // for (let i=0; i<this.cursos.length; i++){
+    //   let curso = this.cursos[i];
+    // }
+
+    // var servico = new CursosService();
+    this.cursos = this.cursosService.getCursos();
   }
 
   ngOnInit() {
