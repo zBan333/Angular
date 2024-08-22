8d2c259cacc918183fadbc488678e8c25d295dc4
Vitor Eble <vitoreble06@gmail.com>
2024-08-22 17:29:29 -0300

    Modulos

---
+++ primeiro-projeto/src/app/cursos/cursos-detalhe/cursos-detalhe.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cursos-detalhe',
+  templateUrl: './cursos-detalhe.component.html',
+  styleUrl: './cursos-detalhe.component.scss'
+})
+export class CursosDetalheComponent {
+
+}
